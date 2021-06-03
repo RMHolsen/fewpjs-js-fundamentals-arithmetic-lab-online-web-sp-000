4066140bf990ee72bdabe1d8436fb7d615f30761
rmholsen <jaguar.kitty@gmail.com>
2021-06-03 15:38:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2 
+
+const random = Math.floor(Math.random() * 10) +1;
+
+let mod = 28 % 6;
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
